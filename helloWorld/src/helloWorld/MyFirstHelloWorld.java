5cc8d066b875815c94720b305a36d66550847cda
Gerhard Pegel <gpegel@gmail.com>
2015-10-22 23:18:20 +0200

    My First Hello World program

--- helloWorld/src/helloWorld/MyFirstHelloWorld.java
+++ helloWorld/src/helloWorld/MyFirstHelloWorld.java
@@ -5,7 +5,7 @@ public class MyFirstHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Program has run succesfully");
-		System.out.println("Hello World!!!");
+		System.out.println("Hello Woooorld!!!");
 
 	}
 
