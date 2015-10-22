85b12a407e7e9f38967e8063ca8ff6a818a9e880
Gerhard Pegel <gpegel@gmail.com>
2015-10-22 22:53:25 +0200

    My First Java program

---
+++ helloWorld/src/helloWorld/MyFirstHelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class MyFirstHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Program has run succesfully");
+
+	}
+
+}
