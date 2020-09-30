974699ddec3d83f22874efb75cfdbb20381d0e1f
Harsh Vardhan omar <64398034+HarshVardhan-Omar@users.noreply.github.com>
2020-09-30 23:35:19 +0530

    Update MyFirstHelloWorld.java
    Suggestion to make the program interactive and explanation of using dynamic input

--- helloWorld/src/helloWorld/MyFirstHelloWorld.java
+++ helloWorld/src/helloWorld/MyFirstHelloWorld.java
@@ -3,6 +3,12 @@ package helloWorld;
 public class MyFirstHelloWorld {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//Make the program interactive ask the user what he/she has to print
+		//Example syntax
+		System.out.println("Enter what to print");
+		String s=sc.nextLine();
+		System.out.println(s);
 		// TODO Auto-generated method stub
 		System.out.println("Program has run succesfully");
 		System.out.println("Hello Woooorld!!!");
